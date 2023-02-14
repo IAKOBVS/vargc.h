f512130d203c86f0dfefecbd0c6b3ee03ded4113
James Halim <tirtajames45@gmail.com>
2023-02-15 00:55:44 +0700

    newest

--- nvtemp/nvtemp.c
+++ nvtemp/nvtemp.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <unistd.h>
 #include "nvtemp.h"
 
 #include <nvidia/gdk/nvml.h>
@@ -19,9 +18,8 @@ int main()
 	unsigned int speed = 36;
 	struct timespec interval;
 	interval.tv_nsec = LOOP_INTERVAL;
-	char buf[57];
 	if (system("sudo nvidia-settings -a \"[gpu:0]/GPUFanControlState=1\"")) goto ERROR_SHUTDOWN;
-	for (;;) {
+	for (char buf[57];;) {
 		if (nvmlDeviceGetTemperature(device, NVML_TEMPERATURE_GPU, &temp) != NVML_SUCCESS) goto ERROR_SHUTDOWN;
 		if (temp - tmp > 2) {
 			switch (temp) {
@@ -36,11 +34,11 @@ int main()
 	if (nvmlShutdown() != NVML_SUCCESS) goto ERROR;
 	system("sudo nvidia-settings -a \"[gpu:0]/GPUFanControlState=0\"");
 	return 0;
+
 ERROR_SHUTDOWN:
 	nvmlShutdown();
 ERROR:
 	system("sudo nvidia-settings -a \"[gpu:0]/GPUFanControlState=0\"");
-	return 0;
 	perror("");
 	return 1;
 }
