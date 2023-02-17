f5596b51723dc7ed5188e80f4ebff05562dc76cc
James Halim <tirtajames45@gmail.com>
2023-02-17 13:46:54 +0700

    newest

--- nvtemp/nvtemp.c
+++ nvtemp/nvtemp.c
@@ -2,8 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 #include "nvtemp.h"
+#include <assert.h>
 
-#include <nvidia/gdk/nvml.h>
+#include </opt/cuda/include/nvml.h>
 
 #define NANO_TO_MILI 1000000
 #define LOOP_INTERVAL 200 * NANO_TO_MILI
@@ -11,32 +12,36 @@
 int main()
 {
 	nvmlDevice_t device;
-	if (nvmlInit() == NVML_SUCCESS); else goto ERROR;
-	if (nvmlDeviceGetHandleByIndex(0, &device) == NVML_SUCCESS); else goto ERROR_SHUTDOWN;
-	int temp;
-	int tmp = 0;
-	unsigned int speed = 36;
-	struct timespec interval;
-	interval.tv_nsec = LOOP_INTERVAL;
-	if (system("sudo nvidia-settings -a \"[gpu:0]/GPUFanControlState=1\"")) goto ERROR_SHUTDOWN;
-	for (char buf[57];;) {
-		if (nvmlDeviceGetTemperature(device, NVML_TEMPERATURE_GPU, (unsigned int *)&temp) == NVML_SUCCESS); else goto ERROR_SHUTDOWN_RESET_FAN;
-		if (abs(tmp - temp) > 2) {
-			switch (temp) {
-				TEMP_THEN_SPEED(speed);
-			}
-			tmp = temp;
-			sprintf(buf, "sudo nvidia-settings -a \"[fan:0]/GPUTargetFanSpeed=%d\"", speed);
-			system(buf);
-		}
-		nanosleep(&interval, NULL);
-	}
-	if (nvmlShutdown() == NVML_SUCCESS); else goto ERROR_SHUTDOWN_RESET_FAN;
-	system("sudo nvidia-settings -a \"[gpu:0]/GPUFanControlState=0\"");
+	if (nvmlInit() == NVML_SUCCESS);
+	else goto ERROR;
+	if (nvmlDeviceGetHandleByIndex(0, &device) == NVML_SUCCESS);
+	else goto ERROR_SHUTDOWN;
+	unsigned int minSpeed;
+	unsigned int maxSpeed;
+	nvmlDeviceGetMinMaxFanSpeed(device, &minSpeed, &maxSpeed);
+	printf("min: %d, max: %d", minSpeed, maxSpeed);
+	/* assert(2==3); */
+	/* int temp; */
+	/* int tmp = 0; */
+	/* unsigned int speed = 36; */
+	/* struct timespec interval; */
+	/* interval.tv_nsec = LOOP_INTERVAL; */
+	/* for (char buf[57];;) { */
+	/* 	if (nvmlDeviceGetTemperature(device, NVML_TEMPERATURE_GPU, (unsigned int *)&temp) == NVML_SUCCESS); */
+	/* 	else goto ERROR_SHUTDOWN; */
+	/* 	if (abs(tmp - temp) > 2) { */
+	/* 		switch (temp) { */
+	/* 			TEMP_THEN_SPEED(speed); */
+	/* 		} */
+	/* 		tmp = temp; */
+	/* 		printf("%d\n", temp); */
+	/* 	} */
+	/* 	nanosleep(&interval, NULL); */
+	/* } */
+	if (nvmlShutdown() == NVML_SUCCESS);
+	else goto ERROR_SHUTDOWN;
 	return 0;
 
-ERROR_SHUTDOWN_RESET_FAN:
-	system("sudo nvidia-settings -a \"[gpu:0]/GPUFanControlState=0\"");
 ERROR_SHUTDOWN:
 	nvmlShutdown();
 ERROR:
