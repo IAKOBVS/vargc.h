ffc135777f212f804e0629df41a87140467f617a
James Halim <tirtajames45@gmail.com>
2023-02-17 14:41:24 +0700

    newest

--- nvtemp/nvtemp.c
+++ nvtemp/nvtemp.c
@@ -9,42 +9,43 @@
 #define NANO_TO_MILI 1000000
 #define LOOP_INTERVAL 200 * NANO_TO_MILI
 
+int nanosleep(const struct timespec *requested_time, struct timespec *remaining);
+
 int main()
 {
+	struct timespec interval;
+	interval.tv_nsec = LOOP_INTERVAL;
 	nvmlDevice_t device;
+	if (system("sudo nvidia-settings -a \"[gpu:0]/GPUFanControlState=1\""))
+		goto ERROR;
 	if (nvmlInit() == NVML_SUCCESS);
 	else goto ERROR;
 	if (nvmlDeviceGetHandleByIndex(0, &device) == NVML_SUCCESS);
 	else goto ERROR_SHUTDOWN;
-	unsigned int minSpeed;
-	unsigned int maxSpeed;
-	nvmlDeviceGetMinMaxFanSpeed(device, &minSpeed, &maxSpeed);
-	printf("min: %d, max: %d", minSpeed, maxSpeed);
-	/* assert(2==3); */
-	/* int temp; */
-	/* int tmp = 0; */
-	/* unsigned int speed = 36; */
-	/* struct timespec interval; */
-	/* interval.tv_nsec = LOOP_INTERVAL; */
-	/* for (char buf[57];;) { */
-	/* 	if (nvmlDeviceGetTemperature(device, NVML_TEMPERATURE_GPU, (unsigned int *)&temp) == NVML_SUCCESS); */
-	/* 	else goto ERROR_SHUTDOWN; */
-	/* 	if (abs(tmp - temp) > 2) { */
-	/* 		switch (temp) { */
-	/* 			TEMP_THEN_SPEED(speed); */
-	/* 		} */
-	/* 		tmp = temp; */
-	/* 		printf("%d\n", temp); */
-	/* 	} */
-	/* 	nanosleep(&interval, NULL); */
-	/* } */
+	int tmp = 0;
+	unsigned int temp;
+	unsigned int speed = 33;
+	for (char buf[57];;) {
+		if (nvmlDeviceGetTemperature(device, NVML_TEMPERATURE_GPU, (unsigned int *)&temp) == NVML_SUCCESS);
+		else goto ERROR_SHUTDOWN;
+		if (abs(tmp - (int)temp) > 2) {
+			switch (temp) { TEMP_THEN_SPEED(speed); }
+			sprintf(buf, "sudo nvidia-settings -a \"[fan:0]/GPUTargetFanSpeed=%d\"", speed);
+			system(buf);
+			tmp = temp;
+		}
+		nanosleep(&interval, NULL);
+	}
 	if (nvmlShutdown() == NVML_SUCCESS);
 	else goto ERROR_SHUTDOWN;
+	system("sudo nvidia-settings -a \"[gpu:0]/GPUFanControlState=0\"");
 	return 0;
 
 ERROR_SHUTDOWN:
 	nvmlShutdown();
 ERROR:
+	if (system("sudo nvidia-settings -a \"[gpu:0]/GPUFanControlState=0\""))
+		goto ERROR;
 	perror("");
 	return 1;
 }
